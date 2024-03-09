cf3e8929193249697a2a5be7d949c06c065dfe1c
espinosaericaella <espinosa.ericaella22@gmail.com>
2024-03-09 15:30:39 +0800

    Initial commit

---
+++ looping.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main(){
+	int x;
+	// void sample() walang nirereturn, walang pinapasa
+	// int sample() may nirereturn, walang pinapasa
+	// void sampl(int x) walang nirereturn, may pinapasa
+	// int sample(intx) may nireretun, may pinapasa
+	
+	
+	/*
+		for(x=1; x<=10; x++){
+			printf("%d ", x);
+	}*/
+	
+	
+	/*
+		for(x=100; x>=0; x-=5){
+			printf("%d ", x);
+	}*/
+	
+	/*
+	x=1;
+	while(x<=10){
+		printf("%d ", x);
+		x++;
+	}*/
+	
+	/*
+	x=0;
+	while(x<=30){
+		printf("%d ", x);
+		x+=3;
+	}*/
+	
+	/*
+	x=0;
+	while(x<=100){
+		printf("%d ", x);
+		x+=5;
+	}*/
+	
+	/*
+	x=100;
+	while(x>=0){
+		printf("%d ", x);
+		x-=5;
+	}*/
+	
+	x=1;
+	do{
+		printf("%d ", x*x);
+		x++;
+	}while(x<=10);
+}
